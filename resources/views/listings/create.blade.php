219b21b8dbe61093a96565da5f954adc1dc45ee5
HM Muneeb <hm.muneeb.u.r@gmail.com>
2022-07-03 05:39:45 +0500

    Added remember old data in form - after validation

--- resources/views/listings/create.blade.php
+++ resources/views/listings/create.blade.php
@@ -25,6 +25,7 @@
                         type="text"
                         class="border border-gray-200 rounded p-2 w-full"
                         name="company"
+                        value = "{{old('company')}}"
                     />
                     @error('company')
                     <p class="text-red-500 text-xs mt-1">{{$message}}</p>
@@ -40,6 +41,7 @@
                         class="border border-gray-200 rounded p-2 w-full"
                         name="title"
                         placeholder="Example: Senior Laravel Developer"
+                        value = "{{old('title')}}"
                     />
                     @error('title')
                     <p class="text-red-500 text-xs mt-1">{{$message}}</p>
@@ -57,6 +59,7 @@
                         class="border border-gray-200 rounded p-2 w-full"
                         name="location"
                         placeholder="Example: Remote, Boston MA, etc"
+                        value = "{{old('location')}}"
                     />
                     @error('location')
                     <p class="text-red-500 text-xs mt-1">{{$message}}</p>
@@ -71,6 +74,7 @@
                         type="text"
                         class="border border-gray-200 rounded p-2 w-full"
                         name="email"
+                        value = "{{old('email')}}"
                     />
                     @error('email')
                     <p class="text-red-500 text-xs mt-1">{{$message}}</p>
@@ -88,6 +92,7 @@
                         type="text"
                         class="border border-gray-200 rounded p-2 w-full"
                         name="website"
+                        value = "{{old('website')}}"
                     />
                     @error('website')
                     <p class="text-red-500 text-xs mt-1">{{$message}}</p>
@@ -103,6 +108,7 @@
                         class="border border-gray-200 rounded p-2 w-full"
                         name="tags"
                         placeholder="Example: Laravel, Backend, Postgres, etc"
+                        value = "{{old('tags')}}"
                     />
                     @error('tags')
                     <p class="text-red-500 text-xs mt-1">{{$message}}</p>
@@ -132,6 +138,7 @@
                         name="description"
                         rows="10"
                         placeholder="Include tasks, requirements, salary, etc"
+                        value = "{{old('description')}}"
                     ></textarea>
                     @error('description')
                     <p class="text-red-500 text-xs mt-1">{{$message}}</p>
